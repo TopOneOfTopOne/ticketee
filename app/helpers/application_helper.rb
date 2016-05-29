fce61baca38d56ff3f6c9cce5f99916c19f9fc22
toponeoftopone <nooblingis@gmail.com>
2016-05-30 02:18:29 +0800

    add title functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts)
+    return false if parts.empty?
+    content_for :title do
+      parts << "Ticketee"
+      parts.join(" - ")
+    end
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,5 +8,10 @@ RSpec.feature "Users can create projects" do
     fill_in "Name", with: "This is my Name"
     fill_in "Description", with: "This is the description"
     click_button "Create Project"
+
+    project = Project.find_by(name: "This is my Name")
+    expect(page.current_url).to eq(project_url(project))
+    title = "This is my Name - Project - Ticketee"
+    expect(page).to have_title title
   end
 end
