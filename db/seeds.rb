65e228bd00e9a75c693983fbf37f32ce199fc899
toponeoftopone <nooblingis@gmail.com>
2016-06-08 16:18:35 +0800

    add seeds + admin attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.exists?(email: "admin@ticketee.com")
+  User.create!(email: "admin@ticketee.com", password: "password", admin: true)
+end
+
+unless User.exists?(email: "viewer@ticketee.com")
+  User.create!(email: "viewer@ticketee.com", password: "password")
+end
+
+%w(Facebook Google Amazon).each do |project|
+  unless Project.exists?(name: project)
+    Project.create!(name: project, description: "The creation of #{project}")
+  end
+end
